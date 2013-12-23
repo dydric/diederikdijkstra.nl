1f530826bf8aa09df0e8b8fcdfbb81eb36d2fed6
Diederik <diederikdijkstra@me.com>
2013-12-23 22:36:06 +0100

    output style

--- config.rb
+++ config.rb
@@ -10,6 +10,7 @@ javascripts_dir = "js"
 fonts_dir = "fonts"
 
 output_style = :compressed
+# output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
