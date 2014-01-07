0e1a7dd0d99a81ce421457ed3a03036992b3d552
Diederik Dijkstra <diederikdijkstra@me.com>
2014-01-07 10:26:09 +0100

    Removed white-space

--- config.rb
+++ config.rb
@@ -19,7 +19,6 @@ relative_assets = true
 # line_comments = false
 color_output = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
