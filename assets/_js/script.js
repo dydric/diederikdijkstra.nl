24dfa4243babea246ddaa1c8f753e6afedbc6dd0
Diederik Dijkstra <diederikdijkstra@gmail.com>
2019-03-13 15:38:54 +0100

    🔥 Remove background

--- assets/_js/script.js
+++ assets/_js/script.js
@@ -3,11 +3,3 @@
 
 twemoji.parse(document.body);
 
-// Get current time
-var bg = document.querySelector('.bg');
-
-if (bg) {
-  var currentdate = new Date();
-  bg.classList.add('bg--H' + currentdate.getHours());
-  // bg.classList.add('bg--H14');
-}
--- assets/js/script.concat.js
+++ assets/js/script.concat.js
@@ -1331,11 +1331,3 @@ return ImagesLoaded;
 
 twemoji.parse(document.body);
 
-// Get current time
-var bg = document.querySelector('.bg');
-
-if (bg) {
-  var currentdate = new Date();
-  bg.classList.add('bg--H' + currentdate.getHours());
-  // bg.classList.add('bg--H14');
-}
