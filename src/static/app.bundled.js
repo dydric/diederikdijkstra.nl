30476d40b2c7fcc3635cea5703f923844c72837c
Diederik <diederikdijkstra@gmail.com>
2020-02-04 16:57:56 +0100

    ✨ Update styling

--- src/static/app.bundled.js
+++ src/static/app.bundled.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){var n;console.log(`I was loaded at ${Date(Date.now()).toString()}`),twemoji.parse(document.body),(n=document.querySelector(".js-toggleMenu"))&&n.addEventListener("click",(function(e){document.body.classList.toggle("show-menu"),e.preventDefault()})),function(){const e=document.querySelectorAll(".js-audio audio");if(e)for(var t=0;t<e.length;t++){e[t].parentNode.querySelector("button").addEventListener("click",(function(e){const t=document.querySelectorAll(".js-audio audio"),n=document.querySelectorAll(".js-audio button"),o=this.parentNode.querySelector("audio");if(o.duration>0&&!o.paused)this.classList.remove("playing"),o.pause();else{for(var r=0;r<n.length;r++)n[r].classList.remove("playing");for(r=0;r<t.length;r++)t[r].pause();this.classList.add("playing"),o.play()}e.preventDefault()}))}}()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/_assets/scripts/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/_assets/scripts/app.js":
+/*!************************************!*\
+  !*** ./src/_assets/scripts/app.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log(`I was loaded at ${Date(Date.now()).toString()}`); // Twemoji\n\ntwemoji.parse(document.body); // Toggle Menu\n\n(function () {\n  var toggleMenuLink = document.querySelector('.js-toggleMenu');\n\n  if (toggleMenuLink) {\n    toggleMenuLink.addEventListener('click', function (e) {\n      document.body.classList.toggle('show-menu');\n      e.preventDefault();\n    });\n  }\n})(); // Audio\n\n\n(function () {\n  const audio = document.querySelectorAll('.js-audio audio');\n\n  if (audio) {\n    for (var i = 0; i < audio.length; i++) {\n      const audioHolder = audio[i].parentNode;\n      const audioButton = audioHolder.querySelector('button');\n      audioButton.addEventListener('click', function (e) {\n        const audio = document.querySelectorAll('.js-audio audio');\n        const button = document.querySelectorAll('.js-audio button');\n        const player = this.parentNode.querySelector('audio');\n\n        if (player.duration > 0 && !player.paused) {\n          this.classList.remove('playing');\n          player.pause();\n        } else {\n          for (var i = 0; i < button.length; i++) {\n            button[i].classList.remove('playing');\n          }\n\n          for (i = 0; i < audio.length; i++) {\n            audio[i].pause();\n          }\n\n          this.classList.add('playing');\n          player.play();\n        }\n\n        e.preventDefault();\n      });\n    }\n  }\n})();\n\n//# sourceURL=webpack:///./src/_assets/scripts/app.js?");
+
+/***/ })
+
+/******/ });
