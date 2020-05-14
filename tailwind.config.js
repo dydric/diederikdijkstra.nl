ed5575a7be55880d8b8c96eeb3d5cbfbc856524d
Diederik <diederikdijkstra@gmail.com>
2020-05-14 21:49:58 +0200

    ✨ Update bookmarks

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
     extend: {
 
       colors: {
+
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+
         accent: {
           'primary': 'var(--color-accent-primary)',
           'secondary': 'var(--color-accent-secondary)'
@@ -17,6 +30,7 @@ module.exports = {
 
       backgroundColor: theme => ({
         'body': 'var(--color-body-background)',
+        'card': 'var(--color-card-background)',
         button: {
           'primary': 'var(--color-button-primary-background)',
           'secondary': 'var(--color-button-secondary-background)',
@@ -25,6 +39,7 @@ module.exports = {
 
       borderColor: theme => ({
         default: theme('colors.gray.300', 'currentColor'),
+        'card': 'var(--color-border-card)',
         'primary': 'var(--color-border-primary)',
         'secondary': 'var(--color-button-secondary)',
       }),
