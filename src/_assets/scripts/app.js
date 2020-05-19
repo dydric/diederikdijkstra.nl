ad4a5f21fb90ea25b2d934c92ed7a812b0e5f734
Diederik <diederikdijkstra@gmail.com>
2020-05-19 09:39:23 +0200

    ✨ Update noise

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -54,3 +54,52 @@ twemoji.parse(document.body);
   }
 
 })();
+
+// Noise
+var manageNoise = {
+  canvas: document.querySelector('.noise'),
+  resize: function () {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+};
+
+var canvas = document.querySelector('.noise');
+var ctx = canvas.getContext('2d');
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function noise(ctx) {
+
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        len = buffer32.length,
+        i = 0;
+
+    for(; i < len;)
+        buffer32[i++] = ((255 * Math.random())|0) << 24;
+
+    ctx.putImageData(idata, 0, 0);
+}
+
+var toggle = true;
+
+// added toggle to get 30 FPS instead of 60 FPS
+function loop() {
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
+    }
+    noise(ctx);
+    requestAnimationFrame(loop);
+};
+
+loop();
