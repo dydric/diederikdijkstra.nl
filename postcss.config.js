a43a0890345473bc4aaf76f25d163b411e85161b
Diederik <diederikdijkstra@gmail.com>
2020-12-14 13:04:36 +0100

    ✨ Update packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
 module.exports = {
+
   plugins: [
     require('postcss-import'),
     require(`tailwindcss`)(`./src/stylesheets/tailwind.config.js`),
     require('postcss-preset-env')({ stage: 1 }),
     require('postcss-nested'),
-    require(`autoprefixer`),
-    ...(process.env.NODE_ENV === "production" ? [
-      require(`postcss-clean`)]
-      : []),
+    require(`autoprefixer`)
   ],
 };
