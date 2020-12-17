8ed0d8fca31a170502d08b1876eae6983e442946
Diederik <diederikdijkstra@gmail.com>
2020-12-17 16:57:26 +0100

    ✨ disable postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require(`tailwindcss`)(`./src/stylesheets/tailwind.config.js`),
-    require('postcss-preset-env')({ stage: 1 }),
+    // require('postcss-preset-env')({ stage: 1 }),
     require('postcss-nested'),
     require(`autoprefixer`)
   ],
