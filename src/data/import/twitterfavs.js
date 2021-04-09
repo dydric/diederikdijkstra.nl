e47043a3021282282e0d0fcd6866861e10ea77fa
Diederik <diederikdijkstra@hey.com>
2021-04-09 20:34:29 +0200

    ✨ Add twitter favorites

---
+++ src/data/import/twitterfavs.js
@@ -0,0 +1,50 @@
+if ( process.env.ELEVENTY_PRODUCTION) {
+
+  const fs = require('fs');
+  const twitter = require('twitter');
+
+  module.exports = () => {
+
+    require('dotenv').config();
+
+    const client = new twitter({
+      consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key:    process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+    let params = {
+      screen_name: 'dydric',
+      count: 200,
+      tweet_mode: 'extended'
+    };
+
+    client.get('favorites/list', params, function (error, tweets) {
+      if (!error) {
+
+        // tweets = tweets.map((tweet) => {
+
+        //   console.log(tweet);
+
+        //   return {
+        //     text:    tweet.text,
+        //     url:     'https://twitter.com/dydric/status/' + tweet.id_str,
+        //     created: tweet.created_at.substring()
+        //   };
+        // });
+
+        // console.log(tweets);
+
+        fs.writeFile(__dirname + '/../twitterfavs.json', JSON.stringify(tweets), err => {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("Updated twitterfavs.json");
+          }
+        });
+      }
+    });
+
+  };
+}
