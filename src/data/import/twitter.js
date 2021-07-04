983b8d27c5b550861c1a6f766b7e5c12c9e4760f
Diederik <diederikdijkstra@hey.com>
2021-07-04 20:27:16 +0200

    ✨ Add tweets

---
+++ src/data/import/twitter.js
@@ -0,0 +1,37 @@
+if ( process.env.ELEVENTY_PRODUCTION) {
+
+  const fs = require('fs');
+  const twitter = require('twitter');
+
+  module.exports = async () => {
+
+    require('dotenv').config();
+
+    const client = new twitter({
+      consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key:    process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+    let params = {
+      screen_name: 'dydric',
+      count: 100,
+      tweet_mode: 'extended'
+    };
+
+    client.get('statuses/user_timeline', params, function (error, favorites) {
+      if (!error) {
+
+        fs.writeFile(__dirname + '/../twitter.json', JSON.stringify(favorites), err => {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("Updated twitter.json");
+          }
+        });
+      }
+    });
+
+  };
+}
